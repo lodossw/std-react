35919f35060908410c6e82369a9472283ebc8e5c
harry <lodossw@gmail.com>
2017-03-15 23:24:49 +0900

    basic event (onclick) 처리

--- public/app.jsx
+++ public/app.jsx
@@ -1,14 +1,31 @@
 class Greeter extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.onButtonClick = this.onButtonClick.bind(this);
+    }
+
+    onButtonClick(event) {
+        event.preventDefault();
+        var name = this.refs.name.value; 
+        alert(name);
+    } 
+
     render() {
-        
         var name = this.props.name;
         var message = this.props.message; 
-
+        
         return ( 
             <div>
               <h1>Hello {name}!!</h1>
               <h1>{message + '!!'}</h1>
+              
+              {/*<form onSubmit={(event) => this.onButtonClick(event)}*/}
+              <form onSubmit={this.onButtonClick}>
+                <input type="text" ref="name"/>
+                <button>Set Name</button>
+              </form> 
+            
             </div>
         );
     }
@@ -19,7 +36,7 @@ Greeter.propTypes = {
   name: React.PropTypes.string
 };
 */
-
+    
 Greeter.defaultProps = {
   name: 'Mary',
   message: 'oops!'
